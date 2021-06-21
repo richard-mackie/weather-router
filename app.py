4a8fa5d25d48b1e6ae8308a68780186dfbf84a37
Richard Mackie <richard.mackie20@gmail.com>
2021-06-21 15:39:26 -0700

    Added leaflet for Openstreet Map

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
-from flask import Flask
+from flask import Flask, url_for, render_template
 
 app = Flask(__name__)
 
-
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
-
+def basemap():
+    return render_template('basemap.html')
 
 if __name__ == '__main__':
     app.run()
