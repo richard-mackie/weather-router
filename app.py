c49e38a119a9e80e3ef193cc3689fb98db8583b6
Richard Mackie <richard.mackie20@gmail.com>
2021-07-01 17:56:09 -0700

    added flask app.py

--- app.py
+++ app.py
@@ -1,10 +1,41 @@
-from flask import Flask, url_for, render_template
+import flask
+from flask import Flask, url_for, render_template, jsonify
+import utils, json
 
 app = Flask(__name__)
 
 @app.route('/')
-def basemap():
-    return render_template('basemap.html')
+def test2():
+    grib_directory = 'static/data/gribs/'
+    grib_file = 'gfs.t12z.pgrb2.1p00.f000'
+    data = utils.create_wind_speed_and_degree_json(grib_directory, grib_file)
+
+    file = open('/home/richard/PycharmProjects/mweatherrouter/static/data/json/gfs.t12z.pgrb2.1p00.f000.json', 'r')
+    jfile = json.load(file)
+    return render_template('json_points_my_example.html', data=jfile)
+
+@app.route('/love')
+def markers():
+    return render_template('index.html')
+
+#https://stackoverflow.com/questions/11178426/how-can-i-pass-data-from-flask-to-javascript-in-a-template
+#https://stackoverflow.com/questions/11178426/how-can-i-pass-data-from-flask-to-javascript-in-a-template
+#https://stackoverflow.com/questions/36167086/separating-html-and-javascript-in-flask/36167179
+
+@app.route('/test')
+def test():
+    grib_directory = 'static/data/gribs/'
+    grib_file = 'gfs.t12z.pgrb2.1p00.f000'
+    data = utils.create_wind_speed_and_degree_json(grib_directory, grib_file)
+
+    file = open('/home/richard/PycharmProjects/mweatherrouter/static/data/json/gfs.t12z.pgrb2.1p00.f000.json', 'r')
+    jfile = json.load(file)
+    return render_template('pass_json_test.html', data=jfile)
+
+@app.route('/test1')
+def test1():
+    data = ['a', 'b', 'c','d']
+    return render_template('settings1.html', data=data, length = len(data))
 
 if __name__ == '__main__':
-    app.run()
+    app.run(use_reloader = True, debug=True)
