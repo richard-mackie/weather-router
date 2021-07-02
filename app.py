d59f21fce8d89452b3efbab57913938da13857fe
Richard Mackie <richard.mackie20@gmail.com>
2021-07-02 07:41:10 -0700

    restricted map to pacific view

--- app.py
+++ app.py
@@ -14,11 +14,6 @@ def test2():
     jfile = json.load(file)
     return render_template('json_points_my_example.html', data=jfile)
 
-@app.route('/love')
-def markers():
-    return render_template('index.html')
-
-#https://stackoverflow.com/questions/11178426/how-can-i-pass-data-from-flask-to-javascript-in-a-template
 #https://stackoverflow.com/questions/11178426/how-can-i-pass-data-from-flask-to-javascript-in-a-template
 #https://stackoverflow.com/questions/36167086/separating-html-and-javascript-in-flask/36167179
 
@@ -31,11 +26,6 @@ def test():
     file = open('/home/richard/PycharmProjects/mweatherrouter/static/data/json/gfs.t12z.pgrb2.1p00.f000.json', 'r')
     jfile = json.load(file)
     return render_template('pass_json_test.html', data=jfile)
-
-@app.route('/test1')
-def test1():
-    data = ['a', 'b', 'c','d']
-    return render_template('settings1.html', data=data, length = len(data))
 
 if __name__ == '__main__':
     app.run(use_reloader = True, debug=True)
