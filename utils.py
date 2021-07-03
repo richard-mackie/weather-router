fe21c26104d6a24114c60d71f0efe5217b745c90
Richard Mackie <richard.mackie20@gmail.com>
2021-07-03 11:23:24 -0700

    looks like gribs are being merged correctly

--- utils.py
+++ utils.py
@@ -1,7 +1,5 @@
 import numpy as np
-import xarray
-import datetime
-import requests
+import glob, os, time, dask, requests, xarray, datetime
 
 def create_wind_speed_and_degree_json(directory, grib_file):
     '''Takes a grib file and converts to a json file'''
@@ -12,20 +10,46 @@ def create_wind_speed_and_degree_json(directory, grib_file):
     with open('./static/data/json/' + grib_file + '.json', 'w') as outfile:
         outfile.write(result)
 
-def get_grib(resolution):
-    url_bas0 = 'https://nomads.ncep.noaa.gov/cgi-bin/filter_gfs_1p00.pl?file=gfs.t00z.pgrb2.1p00.anl&lev_10_m_above_ground=on&var_UGRD=on&var_VGRD=on&leftlon=0&rightlon=360&toplat=90&bottomlat=-90&dir=%2Fgfs.20210702%2F00%2Fatmos'
-    url_bas1 = 'https://nomads.ncep.noaa.gov/cgi-bin/filter_gfs_0p50.pl?file=gfs.t00z.pgrb2full.0p50.f000&lev_10_m_above_ground=on&var_UGRD=on&var_VGRD=on&leftlon=0&rightlon=360&toplat=90&bottomlat=-90&dir=%2Fgfs.20210702%2F00%2Fatmos'
-    url_bas2 = 'https://nomads.ncep.noaa.gov/cgi-bin/filter_gfs_0p50.pl?file=gfs.t06z.pgrb2full.0p50.f000&lev_10_m_above_ground=on&var_UGRD=on&var_VGRD=on&leftlon=0&rightlon=360&toplat=90&bottomlat=-90&dir=%2Fgfs.20210702%2F06%2Fatmos'
-    degrees = {.5:'0p50', 1: '1p00'}
-    #print('https://nomads.ncep.noaa.gov/cgi-bin/filter_gfs_{}.pl?file=gfs.t06z.pgrb2full.0p50.f000&lev_10_m_above_ground=on&var_UGRD=on&var_VGRD=on&leftlon=0&rightlon=360&toplat=90&bottomlat=-90&dir=%2Fgfs.20210702%2F06%2Fatmos'.format(degrees[resolution]))
-    get_grib(.5)
+def get_grib(degrees, left_lon=0, right_lon=360, top_lat=90, bottom_lat=-90, YYYYMMDD='', verbose=False):
+    '''
+    Gets u-10 and v-10 wind grib data from NOAA.
+    https://www.nco.ncep.noaa.gov/pmb/products/gfs/
+    :param resolution:
+    :return:
+    '''
+    grib_directory = 'static/data/gribs/'
 
-date_list = [(datetime.datetime.utcnow() - datetime.timedelta(days=i)) for i in range(7)]
-last_seven_days = [str(day.date()).replace('-','') for day in date_list]
+    DEG = {.25:'0p25', .5:'0p50', 1: '1p00'}
+    # CC is the model cycle runtime (i.e. 00, 06, 12, 18) just using the first one as this doesn't really matter for simulation purposes
+    CC = '00'
+    # FFF is the forecast hour of product from 000 - 384
+    FFF = ['000', '012', '024','036']
+    
+    # YYYYMMDD is the Year, Month and Day
+    if len(YYYYMMDD) == 0:
+        YYYYMMDD = str(datetime.datetime.utcnow().date()).replace('-','')
 
-#grib_directory = 'static/data/gribs/'
-#url = 'https://nomads.ncep.noaa.gov/cgi-bin/filter_gfs_1p00.pl?file=gfs.t00z.pgrb2.1p00.f000&lev_10_m_above_ground=on&var_UGRD=on&var_VGRD=on&leftlon=0&rightlon=360&toplat=90&bottomlat=-90&dir=%2Fgfs.20210702%2F00%2Fatmos'
-#r = requests.get(url, allow_redirects=True)
-#open(grib_directory + last_seven_days[0] + '.grib2', 'wb').write(r.content)
+    for i in range(len(FFF)):
+        url = 'https://nomads.ncep.noaa.gov/cgi-bin/filter_gfs_{}.pl?file=gfs.t00z.pgrb2.1p00.f{}' \
+              '&lev_10_m_above_ground=on&var_UGRD=on&var_VGRD=on&leftlon={}&rightlon={}&toplat={}&bottomlat={}&dir=%2Fgfs.{}%2F00%2Fatmos'.format(DEG[degrees], FFF[i], left_lon, right_lon, top_lat, bottom_lat, YYYYMMDD)
+        if verbose:
+            print(url)
 
-#print(datetime.datetime.utcnow().time())
+        file_name = YYYYMMDD + '.' + DEG[degrees] + '.' + FFF[i] + '.grib2'
+
+        r = requests.get(url, allow_redirects=True)
+        open(grib_directory + file_name, 'wb').write(r.content)
+
+        #data = xarray.open_dataset(grib_directory + file_name, engine='cfgrib')
+        #print(data.u10)
+        time.sleep(1)
+
+#get_grib(degrees=1, right_lon=-90)
+
+grib_directory = './static/data/gribs/'
+os.chdir(grib_directory)
+all_gribs = [grib_directory + file for file in glob.glob('*.grib2')]
+os.chdir('/home/richard/PycharmProjects/mweatherrouter')
+ds = xarray.open_mfdataset(all_gribs, concat_dim='time', combine='nested', engine='cfgrib')
+ds['speed'] = (('time', 'latitude', 'longitude'), np.sqrt(np.square(ds['u10']) + np.square(ds['v10'])))
+ds['degree'] = (('time', 'latitude', 'longitude'), 180 + np.rad2deg(np.arctan2(ds['v10'], ds['u10'])))
