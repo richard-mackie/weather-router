21885a04b8c5b2325ee178a0fcdb7823027a21ff
Richard Mackie <richard.mackie20@gmail.com>
2021-07-02 17:35:50 -0700

    trying to clean up git

--- app.py
+++ app.py
@@ -5,27 +5,30 @@ import utils, json
 app = Flask(__name__)
 
 @app.route('/')
-def test2():
-    grib_directory = 'static/data/gribs/'
-    grib_file = 'gfs.t12z.pgrb2.1p00.f000'
-    data = utils.create_wind_speed_and_degree_json(grib_directory, grib_file)
+def test():
+    grib_directory = './static/data/gribs/'
+    json_directory = './static/data/json/'
+    grib_file = '20210702'
 
-    file = open('/home/richard/PycharmProjects/mweatherrouter/static/data/json/gfs.t12z.pgrb2.1p00.f000.json', 'r')
-    jfile = json.load(file)
-    return render_template('json_points_my_example.html', data=jfile)
+    utils.create_wind_speed_and_degree_json(grib_directory, grib_file)
+    file = open(json_directory + grib_file + '.json', 'r')
+
+    return render_template('json_points_my_example.html', data=json.load(file))
 
 #https://stackoverflow.com/questions/11178426/how-can-i-pass-data-from-flask-to-javascript-in-a-template
 #https://stackoverflow.com/questions/36167086/separating-html-and-javascript-in-flask/36167179
-
 @app.route('/test')
-def test():
+def test1():
     grib_directory = 'static/data/gribs/'
-    grib_file = 'gfs.t12z.pgrb2.1p00.f000'
+    grib_file = '20210702'
     data = utils.create_wind_speed_and_degree_json(grib_directory, grib_file)
 
     file = open('/home/richard/PycharmProjects/mweatherrouter/static/data/json/gfs.t12z.pgrb2.1p00.f000.json', 'r')
     jfile = json.load(file)
+
     return render_template('pass_json_test.html', data=jfile)
 
 if __name__ == '__main__':
     app.run(use_reloader = True, debug=True)
+
+
--- utils.py
+++ utils.py
@@ -1,19 +1,31 @@
 import numpy as np
 import xarray
+import datetime
+import requests
 
 def create_wind_speed_and_degree_json(directory, grib_file):
     '''Takes a grib file and converts to a json file'''
-    ds = xarray.open_dataset(directory + grib_file, engine='cfgrib')
+    ds = xarray.open_dataset(directory + grib_file + '.grib2', engine='cfgrib')
     ds['speed'] = (('latitude', 'longitude'), np.sqrt(np.square(ds['u10']) + np.square(ds['v10'])))
     ds['degree'] = (('latitude', 'longitude'), 180 + np.rad2deg(np.arctan2(ds['v10'], ds['u10'])))
-
     result = ds.to_dataframe()[['speed','degree']].to_json(orient='table', indent=4)
-
     with open('./static/data/json/' + grib_file + '.json', 'w') as outfile:
         outfile.write(result)
 
+def get_grib(resolution):
+    url_bas0 = 'https://nomads.ncep.noaa.gov/cgi-bin/filter_gfs_1p00.pl?file=gfs.t00z.pgrb2.1p00.anl&lev_10_m_above_ground=on&var_UGRD=on&var_VGRD=on&leftlon=0&rightlon=360&toplat=90&bottomlat=-90&dir=%2Fgfs.20210702%2F00%2Fatmos'
+    url_bas1 = 'https://nomads.ncep.noaa.gov/cgi-bin/filter_gfs_0p50.pl?file=gfs.t00z.pgrb2full.0p50.f000&lev_10_m_above_ground=on&var_UGRD=on&var_VGRD=on&leftlon=0&rightlon=360&toplat=90&bottomlat=-90&dir=%2Fgfs.20210702%2F00%2Fatmos'
+    url_bas2 = 'https://nomads.ncep.noaa.gov/cgi-bin/filter_gfs_0p50.pl?file=gfs.t06z.pgrb2full.0p50.f000&lev_10_m_above_ground=on&var_UGRD=on&var_VGRD=on&leftlon=0&rightlon=360&toplat=90&bottomlat=-90&dir=%2Fgfs.20210702%2F06%2Fatmos'
+    degrees = {.5:'0p50', 1: '1p00'}
+    #print('https://nomads.ncep.noaa.gov/cgi-bin/filter_gfs_{}.pl?file=gfs.t06z.pgrb2full.0p50.f000&lev_10_m_above_ground=on&var_UGRD=on&var_VGRD=on&leftlon=0&rightlon=360&toplat=90&bottomlat=-90&dir=%2Fgfs.20210702%2F06%2Fatmos'.format(degrees[resolution]))
+    get_grib(.5)
 
-grib_directory = 'static/data/gribs/'
-grib_file = 'gfs.t12z.pgrb2.1p00.f000'
+date_list = [(datetime.datetime.utcnow() - datetime.timedelta(days=i)) for i in range(7)]
+last_seven_days = [str(day.date()).replace('-','') for day in date_list]
 
-create_wind_speed_and_degree_json(grib_directory, grib_file)
+#grib_directory = 'static/data/gribs/'
+#url = 'https://nomads.ncep.noaa.gov/cgi-bin/filter_gfs_1p00.pl?file=gfs.t00z.pgrb2.1p00.f000&lev_10_m_above_ground=on&var_UGRD=on&var_VGRD=on&leftlon=0&rightlon=360&toplat=90&bottomlat=-90&dir=%2Fgfs.20210702%2F00%2Fatmos'
+#r = requests.get(url, allow_redirects=True)
+#open(grib_directory + last_seven_days[0] + '.grib2', 'wb').write(r.content)
+
+#print(datetime.datetime.utcnow().time())
