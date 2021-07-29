53cfd104103e6c22d58edc65608a9239f93d4690
Richard Mackie <richard.mackie20@gmail.com>
2021-07-29 11:01:57 -0700

    debugging heroku with 25 sec timeout

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class Config(object):
     debug = True
 
     # Optimal route timeout
-    timeout = 60
+    timeout = 25
 
     # This is the mapping extent NorthEast Corner, SouthWest Corner
     extents = { 'lat': 52.56928286558243, 'lng': -95.88867187500001 }, { 'lat': 17.26672782352052, 'lng': -177.09960937500003 }
